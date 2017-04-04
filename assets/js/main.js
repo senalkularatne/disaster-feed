dc0d55d6a188495c3477681972ae3a718ee7b18d
Senal Kularatne <senalk90@gmail.com>
2017-04-04 01:25:38 -0400

    Fixed issues with maginitude not displaying properly, added new stuff to resources

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,26 @@
-var hourBtn = document.getElementById("btn-hour");
-var dayBtn = document.getElementById("btn-1day");
-var sevenDaysBtn = document.getElementById("btn-7days");
-var thirtyDaysBtn = document.getElementById("btn-30days");
+
+// document.getElementById("date-time").innerHTML = Date();
+
+// tday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+// tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+
+// function GetClock(){
+// var d=new Date();
+// var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getYear();
+// if(nyear<1000) nyear+=1900;
+// var nhour=d.getHours(),nmin=d.getMinutes(),ap;
+// if(nhour==0){ap=" AM";nhour=12;}
+// else if(nhour<12){ap=" AM";}
+// else if(nhour==12){ap=" PM";}
+// else if(nhour>12){ap=" PM";nhour-=12;}
+
+// if(nmin<=9) nmin="0"+nmin;
+
+// document.getElementById('clockbox').innerHTML=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+", "+nyear+" "+nhour+":"+nmin+ap+"";
+// }
+
+// window.onload=function(){
+// GetClock();
+// setInterval(GetClock,1000);
+// }
+
--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,32 +1,38 @@
-
-// **************** Notes ****************
+// **********************
+// 	       Notes 
+// **********************
 
 // Get token from here https://www.mapbox.com/studio/account/tokens/
 
-// Meaning of the numbers used in the documenation example (https://www.mapbox.com/api-documentation/#static)
-// Retrieve a map at -122.4241 longitude, 37.78 latitude.
-// Zoom 14.24, bearing 0, and pitch 60. 
-// The map will be 600 pixels wide and 600 pixels high.
-
-// Things changed from the example
-// streets-v8 --> dark-v9 This will change the map into dark mode
-// Change the pitch from 60 to 0 to flatten the map from the angle
-// The default view is somewhere in San Francisco so to obtain the world map change the longitude and latitude. I changed it to 0,0
-// Change the zoom level to 1
-// Since we want a wider view of the world map change 600x600 to 1024x512
+// Meaning of the numbers used in loadImage (https://www.mapbox.com/api-documentation/#static)
+	// latitude of the center point of the static map
+	// longitude of the center point of the static map
+	// zoom level (0-22)
+	// bearing - rotates the map around its center (0-360 degrees)
+	// pitch - tilts the map (0 and  60 degrees)
+	// The map will be 600 pixels wide and 600 pixels high.
 
 // lonitude = x values
 // latitude = y values
 
-// Range of these x y values are in angles of ration
+// Range of these x y values are in angles of rotion
 // Example: If you Google latitude and longitude of NYC it is 40.7128° N, 74.0059° W
 // Refer to the globe image in the Resources folder
 
-// **************** End Notes ****************
+// Resources:
+// Earthquake Data: https://earthquake.usgs.gov/earthquakes/feed/v1.0/csv.php
 
-// **************** Variables ****************
+// How latitude and longitude works:
+// Example 1. 31.2304° N, 121.4737° E - Shanghai, China
+// Example 2. 49.2827° N, 123.1207° W - Vancouvar, Canada. 
+// W 123.1207 is changed to -123.1207. Look at the globe image in resources to understand more.
 
-var mapImage; // Store the loaded image
+// **********************
+// 	     Variables 
+// **********************
+
+// Store the loaded image
+var mapImage; 
 
 // How data is split in the cvs file: time stamp, latitude, longitude, someother data, magnitude of the eathquake
 var earthquakeData_1Hour;
@@ -37,29 +43,34 @@ var earthquakeData_30Days;
 var centerLatitude = 0;
 var centerLongitude = 0;
 
-// 31.2304° N, 121.4737° E - Shanghai, China
-// 49.2827° N, 123.1207° W - Vancouvar, Canada. In this example since it is W 123.1207 is changed to -123.1207. Look at the globe image in resources to understand more.
-
-// These measurements are in degrees and must be converted to radians. webMercatorX and webMercatorY takes values in radians.
+// These measurements are in degrees and must be converted to radians. 
+// webMercatorX and webMercatorY functions handles the conversion
 var latitude = 49.2827;
 var longitude = -123.1207;
 
 var zoom = 1;
 
-// **************** End Variables ****************
+// **********************************************
+// 	  Load Map Image & earthquake data
+// **********************************************
 
 function preload(){
 	// Image (from mapbox) is loaded and displayed to the screen using the loadImage function from p5.js
 	mapImage = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/0,0,1,0,0/1024x512?access_token=pk.eyJ1Ijoic2VuYWxrdWxhIiwiYSI6ImNqMGltb251ZjAwazMycW1lZ3NvdmF3NzUifQ.IWW29a6TlrwU5Arm-Aym3A');
 	
-	// earthquakeData_1Day = loadStrings('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.csv'); // A csc file is being loaded. It is a comma seperated value file. 
+	// A csv file is being loaded. It is a comma seperated value file. 
+	earthquakeData_1Hour = loadStrings('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.csv');
+	earthquakeData_1Day = loadStrings('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.csv');
+	earthquakeData_7Days = loadStrings('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.csv');
 	earthquakeData_30Days = loadStrings('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv');
 }
 
-// **************** Formula ****************
+// **********************
+// 	     Formula 
+// **********************
 
-// Convert latitude and longitude to x y values
-// Apply the formula here (https://en.wikipedia.org/wiki/Web_Mercator --> Formulas)
+// Convert latitude and longitude to that and in degrees to radians
+// Apply the formula from here (https://en.wikipedia.org/wiki/Web_Mercator --> Formulas)
 // λ = longitude in radians 
 // φ = latitude in radians
 
@@ -75,29 +86,56 @@ function webMercatorY(latitude) {
 	var y =  ((256 / PI) * pow(2, zoom)) * (PI - log(tan(PI/4 + latitude/2)));
 	return y;
 }
-// **************** End Formula ****************
+
+// **********************************************
+// 	         	         Buttons
+// **********************************************
+
+var hourBtn = document.getElementById("btn-hour");
+var dayBtn = document.getElementById("btn-1day");
+var sevenDaysBtn = document.getElementById("btn-7days");
+var thirtyDaysBtn = document.getElementById("btn-30days");
+
+
+hourBtn.onclick = function() {
+	alert("hourBtn button was clicked");
+};
+
+dayBtn.onclick = function() {
+	alert("dayBtn button was clicked");
+};
+
+sevenDaysBtn.onclick = function() {
+	alert("sevenDaysBtn button was clicked");
+};
+
+thirtyDaysBtn.onclick = function() {
+	alert("thirtyDaysBtn button was clicked");
+};
+
+// **********************************************
+// 	         Setup the earthquake data
+// **********************************************
 
 function setup() {
-	// p5.js - Creates a canvas element in the document, and sets the dimensions of it in pixels.
+	// Creates a P5.js canvas and set the dimension (pixels). The number must match the number at mapImage = loadImage('')
 	createCanvas (1024, 512);
 
-	// We do this so we can work from the center
-	// This moves the origin from top to center
-	// This will make things easier as the values we get are relative to the center
+	// This centers the map image (Refer resources --> Centering)
 	translate(width/2, height/2);
 	imageMode(CENTER);
 
-	// Draw an image to the main canvas of the p5js sketch
-	// Format : image(img,x,y,width,height)
-	// img - image to display
-	// x - Number: the x-coordinate at which to place the top-left corner of the source image
-	// y - Number: the y-coordinate at which to place the top-left corner of the source image
+	// Draw an image to p5js canvas. Format : image(img,x,y,width,height)
+		// img - image to display
+		// x - Number: the x-coordinate at which to place the top-left corner of the source image
+		// y - Number: the y-coordinate at which to place the top-left corner of the source image
 	image(mapImage,0,0);
 
+	// These are centerlongitude and centerlatidue in radians
 	var centerX = webMercatorX(centerLongitude);
 	var centerY = webMercatorY(centerLatitude);
 
-	for (var i =0; i< earthquakeData_30Days.length; i++){
+	for (var i = 0; i< earthquakeData_30Days.length; i++){
 		// split() breaks a String into pieces using a character or string as the delimiter. 
 		// split(value,delim). 
 		// value --> the String to be split
@@ -109,32 +147,42 @@ function setup() {
 		var latitudeData = data[1];
 		var longitudeData = data[2];
 
+		// These are longitude and latidue in radians
 		// - centerX and and -centerY is doen because x and y values are the difference between the position of the country and the center
 		var x = webMercatorX(longitudeData) - centerX;
 		var y = webMercatorY(latitudeData) - centerY;
 
+		
+
+		// *********************
+		// 	     Magnitude
+		// *********************
 		magnitude = pow(10, magnitude);
 		magnitude = sqrt(magnitude);
 
-		var maxMagnitude = sqrt(pow(10,10));
+		var  maxMagnitude = sqrt(pow(10,10));	
 
-		// Make a circle (ellipse)
-		fill(0,255,0, 70); // Sets the color used to fill shapes in the format rgba
-		ellipse(x, y, 8, 8); // Draw an oval. x-coordinate, y-coordinate, width and height of ellipse
+		var diameter = map(magnitude, 0, maxMagnitude, 0, 1800);
+
+		// Sets color (rgba)
+		fill(0, 255, 0, 70); 
+		// Draw an oval. x-coordinate, y-coordinate, width and height of ellipse
+		ellipse(x, y, diameter, diameter); 
+		// Color to put around circle
+		stroke(0, 255, 0, 70);	
+
 	}
 
+	// These are longitude and latidue in radians
 	// - centerX and and -centerY is doen because x and y values are the difference between the position of the country and the center
-	var x = webMercatorX(longitude) - centerX;
-	var y = webMercatorY(latitude) - centerY;
+	// var x = webMercatorX(longitude) - centerX;
+	// var y = webMercatorY(latitude) - centerY;
 
-	var diameter = map(magnitude, 0 , 10, 0, 60);
-	stroke(0,255,0, 70);
 	// Make a circle (ellipse)
-	fill(0,255,0, 70); // Sets the color used to fill shapes in the format rgba
-	ellipse(x, y, diameter, diameter); // Draw an oval. x-coordinate, y-coordinate, width and height of ellipse
-
+	// fill(0,255,0, 70); // Sets the color used to fill shapes in the format rgba
+	// ellipse(x, y, diameter, diameter); // Draw an oval. x-coordinate, y-coordinate, width and height of ellipse
 }
 
-function draw() {
 
-}
+
+
